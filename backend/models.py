ec44058d93cf2003aaa66e96506987e76bbd7b24
Ziyad Bin Ayoobkhan P <163715891+ZiyadBin@users.noreply.github.com>
2025-11-18 10:38:08 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -5,8 +5,8 @@ class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   username = db.Column(db.String(80), unique=True, nullable=False)
   password_hash = db.Column(db.String(128), nullable=False)
-    # This creates a 'user.transactions' list on a user object
-    transactions = db.relationship('Transaction', backref='user', lazy=True, cascade="all, delete-orphan")
+  # This creates a 'user.transactions' list on a user object
+  transactions = db.relationship('Transaction', backref='user', lazy=True, cascade="all, delete-orphan")
     def set_password(self, password):
         """Hashes and sets the password."""
         self.password_hash = bcrypt.generate_password_hash(password).decode('utf-8')
