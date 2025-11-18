ecb8c265285cfe8e528c2ad8718cf2df588f6a7c
Ziyad Bin Ayoobkhan P <163715891+ZiyadBin@users.noreply.github.com>
2025-11-18 11:35:33 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,6 @@ import os
 from datetime import timedelta
 
 class Config:
-    # These lines must be indented
     SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key-here')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-dev-secret-here')
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
@@ -11,10 +10,16 @@ class Config:
     # Get the database URL from Render
     db_url = os.environ.get('DATABASE_URL')
     
-    # Tell SQLAlchemy to use the new 'psycopg' (v3) driver
-    if db_url and db_url.startswith("postgres://"):
+    # --- NEW DEBUGGING CODE ---
+    if db_url is None:
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("!!!! FATAL ERROR: DATABASE_URL environment variable is NOT SET. !!!!")
+        print("!!!! Please add it in the Render dashboard. !!!!")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    elif db_url.startswith("postgres://"):
+        # This is the correct path:
+        print("+++ DATABASE_URL found. Applying psycopg driver. +++")
         db_url = db_url.replace("postgres://", "postgresql+psycopg://", 1)
     
-    # These lines must also be indented
     SQLALCHEMY_DATABASE_URI = db_url
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # Disables a warning
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
